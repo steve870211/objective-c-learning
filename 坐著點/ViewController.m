8483207aafcef4b5265f652039f9ae8ad7d29c42
steve870211 <steve870211@gmail.com>
2016-03-18 09:36:22 +0800

    從mySQL撈資料 number欄位無法正常顯示問題待解決

--- 坐著點/ViewController.m
+++ 坐著點/ViewController.m
@@ -10,67 +10,45 @@
 #import "Note.h"
 #import "TheTableViewCell.h"
 
-@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@interface ViewController ()<UITableViewDataSource>
+{
+
+}
+
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic) NSMutableArray *Menus;
-
+@property (nonatomic) NSMutableArray *the_arr;
 
 @end
 
 @implementation ViewController
 
+
 - (instancetype)initWithCoder:(NSCoder *)aDecoder
 {
-    NSMutableArray<Note*> *mcdonaldmenus = [[NSMutableArray alloc]init];
-    NSMutableArray<Note*> *KFCmenu = [[NSMutableArray alloc]init];
     self = [super initWithCoder:aDecoder];
-    
-    NSArray *foodNames = @[@"麥克雞塊餐",@"麥脆雞餐",@"雙層牛肉起士堡",@"三層牛肉超級堡",@"太空米漢堡",@"北海鱈魚堡"];
-    NSArray *price = @[@"125",@"129",@"125",@"135",@"111",@"150"];
-    for(int i=0 ; i < foodNames.count ; i++){
-        Note * menu = [[Note alloc]init];
-        menu.FoodName = foodNames[i];
-        menu.Price = price[i];
-        menu.Number = @(0);
-        [mcdonaldmenus addObject:menu];
-    }
-    
-    NSArray * foodNames2 = @[@"六塊🐔餐", @"三號全家餐", @"蛋塔禮盒", @"辣味脆雞", @"薄皮嫩雞"];
-    NSArray * price2 = @[@"199",@"299",@"329",@"129",@"129"];
-    for(int i=0 ; i < foodNames2.count ; i++){
-        Note * menu = [[Note alloc]init];
-        menu.FoodName = foodNames2[i];
-        menu.Price = price2[i];
-        menu.Number = @(0);
-        [KFCmenu addObject:menu];
-    }
-    
-    _Menus = [[NSMutableArray alloc]initWithObjects:mcdonaldmenus,KFCmenu, nil];
     
     return self;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self the_reload_model];
     self.tableView.dataSource = self;
-    self.tableView.delegate = self;
+    _Menus = [[NSMutableArray alloc]initWithObjects:_the_arr, nil];
     
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-
+    
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     switch (section) {
-        case 0:
-            return @"麥當勞";
-            break;
             
-        case 1:
-            return @"肯德基";
+        case 0:
+            return @"四海遊龍";
             break;
             
         default:
@@ -82,14 +60,16 @@
 // Section的數量
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     
-    return _Menus.count;
+    return 1;
     
 }
 
 // Cell的數量
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
  
-    return [[_Menus objectAtIndex:section] count];
+//    return _the_arr.count;
+//    return [[_Menus objectAtIndex:section] count];
+    return _Menus.count;
     
 }
 
@@ -97,7 +77,8 @@
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
     TheTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    Note *note = _Menus[indexPath.section][indexPath.row];
+//    Note *note = _Menus[indexPath.section][indexPath.row];
+    Note *note = _Menus[indexPath.row];
 //    cell.shopname.text = note.ShopName;
     cell.foodname.text = note.FoodName;
     cell.price.text = note.Price;
@@ -108,7 +89,84 @@
     cell.number.tag = indexPath.row;
     cell.note = note;
     
+//    TheTableViewCell *cell;
+//    cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+//    NSDictionary *er;
+//    er = _the_arr[indexPath.row];
+//    cell.foodname.text = er[@"foodName"];
+//    cell.price.text = er[@"price"];
+//    cell.number = 0;
+    
     return cell;
+    
+}
+
+-(void)the_reload_model{
+    
+    NSURL *url = [NSURL URLWithString:@"http://localhost:8888/myPHP/OrderEasy.php"];
+    NSMutableURLRequest *request;
+    request = [NSMutableURLRequest requestWithURL:url];
+    NSURLSessionConfiguration *config;
+    config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session;
+    session = [NSURLSession sessionWithConfiguration:config delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *dataTask;
+    dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error != nil) {
+            
+            UIAlertController *alert;
+            alert = [UIAlertController new];
+            UIAlertAction * alertAct;
+            alertAct = [UIAlertAction actionWithTitle:@"連不上" style:UIAlertActionStyleDefault handler:nil];
+            [alert addAction:alertAct];
+            [self presentViewController:alert animated:true completion:nil];
+            
+        } else {
+            
+            NSString *con = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+            
+            NSLog(@"josn=%@", con);
+            
+            NSArray * arr = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            
+            _the_arr = [NSMutableArray arrayWithArray:arr];
+            
+            for (int i=0; i<_the_arr.count; i++) {
+                
+                NSDictionary *book = _the_arr[i];
+                Note* note = [Note new];
+                note.ShopName = book[@"shopName"];
+                note.FoodName = book[@"foodName"];
+                note.Price = book[@"price"];
+                
+                [self.Menus addObject:note];
+                
+            }
+
+            
+            
+            if (_the_arr) {
+                
+                [_tableView reloadData];
+                
+            } else {
+                
+                UIAlertController *alert;
+                alert = [UIAlertController new];
+                UIAlertAction * alertAct;
+                alertAct = [UIAlertAction actionWithTitle:@"MySQL有問題" style:UIAlertActionStyleDefault handler:nil];
+                [alert addAction:alertAct];
+                [self presentViewController:alert animated:true completion:nil];
+                
+            }
+            
+            NSLog(@"連上囉");
+        }
+        
+    }];
+    
+    [dataTask resume];
     
 }
 
@@ -118,7 +176,8 @@
     CGPoint buttonPosition = [sender convertPoint:CGPointZero toView:self.tableView];
     NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:buttonPosition];
     
-    Note * menu = _Menus[indexPath.section][indexPath.row];
+//    Note * menu = _Menus[indexPath.section][indexPath.row];
+    Note *menu = _Menus[indexPath.row];
     
     NSNumber *a = menu.Number;
     if ( a.intValue < 99) {
@@ -136,7 +195,8 @@
     CGPoint buttonPosition = [sender convertPoint:CGPointZero toView:self.tableView];
     NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:buttonPosition];
     
-    Note * menu = _Menus[indexPath.section][indexPath.row];
+//    Note * menu = _Menus[indexPath.section][indexPath.row];
+    Note *menu = _Menus[indexPath.row];
     
     NSNumber *a = menu.Number;
     if ( a.intValue > 0 ) {
@@ -146,6 +206,12 @@
 //    NSLog(@"%@",a);
     [self.tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 
+}
+
+- (IBAction)resetBtn:(id)sender {
+    
+    
+    
 }
 
 
